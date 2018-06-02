4279cd0a20ffd1bff4774ea8bd86e74a03633b58
Jonathan Nicholas <jonicrecis@gmail.com>
2018-06-03 09:09:44 +1000

    multiple subreddit support

--- reddit_streaming.py
+++ reddit_streaming.py
@@ -4,13 +4,16 @@ from getarticle import get_article
 from persistence import *
 from makecomment import make_comment
 
-reddit = praw.Reddit(user_agent="Allkpop_scraper",client_id=OAUTH_CLIENT,client_secret=OAUTH_SECRET,username=USERNAME,password=PASSWORD)
-subreddit = reddit.subreddit(SUBREDDIT)
-urls = list_urls(FILENAME)
-for submission in subreddit.stream.submissions():
-    current_submission = reddit.submission(id=submission)
-    url = current_submission.url
-    if url not in urls and "allkpop" in url:
-        result_set = get_article(url)
-        make_comment(current_submission,result_set)
-        append_url(FILENAME,url)
+def main():
+    reddit = praw.Reddit(user_agent="Allkpop_scraper",client_id=OAUTH_CLIENT,client_secret=OAUTH_SECRET,username=USERNAME,password=PASSWORD)
+    subreddit = reddit.subreddit(SUBREDDIT)
+    urls = list_urls(FILENAME)
+    for submission in subreddit.stream.submissions():
+        current_submission = reddit.submission(id=submission)
+        url = current_submission.url
+        current_url = "{}:{}".format(current_submission.subreddit,url)
+        if url not in urls and "allkpop" in url:
+            result_set = get_article(url)
+            make_comment(current_submission,result_set)
+            urls.append(current_url)
+            append_url(FILENAME,current_url)
