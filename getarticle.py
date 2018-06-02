a1e8aa733dd3048f22ceb8862ca6012ca5892e15
Jonathan Nicholas <jonicrecis@gmail.com>
2018-06-03 09:09:31 +1000

    formatting-related fixes

--- getarticle.py
+++ getarticle.py
@@ -1,17 +1,45 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
+from template import WHITESPACE
+
+def image_set_format(image_set):
+    string = ""
+    for i in range(len(image_set)):
+        string += "[{}]({}) ".format(i+1,image_set[i])
+    return string
+
+def article_formatting(article):
+    string = ""
+    previous = ''
+    for part in article:
+        if (part == '' or part == ' ') and previous == '':
+            pass
+        elif part == '' and previous != '':
+            string += WHITESPACE
+        else:
+            string += part
+        previous = part
+    return string
 
 def get_article(url):
     page = urlopen(Request(url, headers={'User-Agent': 'Mozilla'}))
     soup = BeautifulSoup(page,"html.parser")
     title = soup.title.get_text()
-    title_image = soup.find("img",id="article-image").get("src")
+    try:
+        title_image = soup.find("img",id="article-image").get("src")
+    except AttributeError:
+        title_image = ""
     #remove SEE ALSO and tags at the bottom before used by the article div
     soup.find(style="font-size:16px!important;font-weight:bold!important;").decompose()
     soup.find("div",id="article-headline-tags").decompose()
     article_div = soup.find("div",class_="entry_content")
-    article = article_div.get_text().strip().replace(u'\xa0', u' ')
+    article_div_div = article_div.find_all("div")
+    text = [div.get_text().replace(u'\xa0', u' ') for div in article_div_div][:-1]
+    for div in article_div_div:
+        div.decompose() #this is disgusting, but so is web scraping
+    first_line = soup.find("div",class_="entry_content").get_text().strip()
+    article = article_formatting([first_line.replace(u'\xa0', u' ')] + text)
     images = [link.get('src') for link in article_div.find_all("img")]
-    image_set = ''.join(images)
+    image_set = image_set_format(images)
     result_set = {'title':title,'title_image':title_image,'article':article,'images': image_set}
     return result_set
--- template.py
+++ template.py
@@ -5,9 +5,13 @@ TEMPLATE = """
 
 {}
 
-{}
+Images: {}
 
 *****
 
 *I am a bot that transcribes allkpop articles. PM /u/jonicrecis for feedback.*
+"""
+
+WHITESPACE = """
+
 """
