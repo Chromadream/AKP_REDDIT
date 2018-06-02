c01b8f919471fd620bf7157b3d20a3b33ff9bd34
Jonathan Nicholas <jonicrecis@gmail.com>
2018-06-02 18:13:51 +1000

    persistence related stuff
    ensuring no duplicate entry even after there's a downtime on server

---
+++ persistence.py
@@ -0,0 +1,10 @@
+def list_urls(filename):
+    persistence_file = open(filename,'r')
+    urllist = persistence_file.readlines()
+    persistence_file.close()
+    return urllist
+
+def append_url(filename,url):
+    persistence_file = open(filename,'a')
+    persistence_file.write(url)
+    persistence_file.close()
--- reddit_streaming.py
+++ reddit_streaming.py
@@ -1,13 +1,16 @@
 import praw
 from CONFIG import *
 from getarticle import get_article
+from persistence import *
 from makecomment import make_comment
 
 reddit = praw.Reddit(user_agent="Allkpop_scraper",client_id=OAUTH_CLIENT,client_secret=OAUTH_SECRET,username=USERNAME,password=PASSWORD)
 subreddit = reddit.subreddit(SUBREDDIT)
+urls = list_urls(FILENAME)
 for submission in subreddit.stream.submissions():
     current_submission = reddit.submission(id=submission)
     url = current_submission.url
-    if "allkpop" in url:
+    if url not in urls and "allkpop" in url:
         result_set = get_article(url)
         make_comment(current_submission,result_set)
+        append_url(FILENAME,url)
