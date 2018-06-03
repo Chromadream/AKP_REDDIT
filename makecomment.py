b3635710ec550cab0cbe362ab7a3f02659efbec5
Jonathan Nicholas <jonicrecis@gmail.com>
2018-06-03 11:24:07 +1000

    formatting modularity

--- makecomment.py
+++ makecomment.py
@@ -2,9 +2,11 @@ import praw
 import time
 from template import TEMPLATE
 
+def formatting(result_set):
+    return TEMPLATE.format(result_set['title'],result_set['title_image'],result_set['article'],result_set['images'])
+
 def make_comment(current_submission, result_set):
-    formatted_reply = TEMPLATE.format(result_set['title'],result_set['title_image'],result_set['article'],result_set['images'])
-    print(formatted_reply)
+    formatted_reply = formatting(result_set)
     try:
         current_submission.reply(formatted_reply)
     except praw.exceptions.APIException:
