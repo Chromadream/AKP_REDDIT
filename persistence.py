e44e0dd9e6d150f57cdbb09d2048890416aa8826
Jonathan Nicholas <jonicrecis@gmail.com>
2018-06-03 11:24:36 +1000

    persistence related fixes

--- persistence.py
+++ persistence.py
@@ -1,11 +1,11 @@
 def list_urls(filename):
     persistence_file = open(filename,'r')
-    urllist = persistence_file.readlines()
+    urllist = [url.rstrip() for url in persistence_file.readlines()]
     persistence_file.close()
     return urllist
 
 def append_url(filename,url):
     persistence_file = open(filename,'a')
-    persistence_file.write(url)
     persistence_file.write("\n")
+    persistence_file.write(url)
     persistence_file.close()
