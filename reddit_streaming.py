512d086e338dc62ebf5a70fc1857d8d52591c512
Jonathan Nicholas <jonicrecis@gmail.com>
2018-06-03 13:39:04 +1000

    no retroactive scraping

--- reddit_streaming.py
+++ reddit_streaming.py
@@ -3,6 +3,7 @@ from CONFIG import *
 from getarticle import get_article
 from persistence import *
 from makecomment import make_comment
+import time
 
 def fetch_new_URLs(domain_instance):
     return domain_instance.new()
@@ -11,13 +12,14 @@ def main():
     reddit = praw.Reddit(user_agent="Allkpop_scraper",client_id=OAUTH_CLIENT,client_secret=OAUTH_SECRET,username=USERNAME,password=PASSWORD)
     domain = reddit.domain('allkpop.com')
     urls = list_urls(FILENAME)
+    instance_start = time.time()
     while True:
         dataset = fetch_new_URLs(domain)
         for submission in dataset:
             current_submission = reddit.submission(id=submission)
             url = current_submission.url
             current_url = "{}:{}".format(current_submission.subreddit,url)
-            if current_url not in urls:
+            if current_url not in urls and current_submission.created_utc > instance_start:
                 result_set = get_article(url)
                 make_comment(current_submission,result_set)
                 print("Success? {}".format(current_url))
