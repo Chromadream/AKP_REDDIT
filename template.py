eabf9f2da286cead6485011963901a15c8d7b49b
Jonathan Nicholas <jonicrecis@gmail.com>
2018-06-05 13:01:03 +1000

    Footer update

--- template.py
+++ template.py
@@ -10,6 +10,8 @@ TEMPLATE = """
 *****
 
 *I am a bot that transcribes allkpop articles. PM this bot for feedback.*
+
+*[Reason why this bot exists. (alternate title: Why you should not give any clicks to allkpop)](https://twitter.com/crushyeols/status/947982539548962817?s=19)*
 """
 
 WHITESPACE = """
