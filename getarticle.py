00a0cb4f2e54c1ecc6c760240fab9dd2e613b2c8
notatrueroute <notatrueroute@gmail.com>
2019-01-08 17:54:29 -0600

    Parse hyperlink/bold/italics. Get embedded youtube links. Grep image filename.

--- getarticle.py
+++ getarticle.py
@@ -2,32 +2,69 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup, element
 from template import WHITESPACE, AKP_DOMAIN
 
+def get_youtube(soup_elem):
+    link  = soup_elem.attrs['src'].replace('?','&').replace('embed/', 'watch/?v=')
+    title = ""
+    try:
+        page = urlopen(Request(link, headers={'User-Agent': 'Mozilla'}))
+        soup = BeautifulSoup(page,"html.parser")
+        title = soup.title.get_text()
+        if title == "YouTube":
+            title = "Broken Video"
+        else:
+            title = title.replace(" - YouTube", "")
+        title = "YouTube: " + title
+
+    except ValueError:
+        pass    # invalid link URL
+    return title, link
 
 def get_text_wrapper(soup_elem):
-    return get_text(soup_elem)[0].replace(u"\xa0", u" ").strip()
+    return get_text(soup_elem).replace(u'\xa0', u' ').strip()
 
-
-def get_text(soup_elem, img_count=0):
+def get_text(soup_elem):
     buff = ""
     if isinstance(soup_elem, element.NavigableString):
         buff += soup_elem
     elif isinstance(soup_elem, element.Tag):
-        if soup_elem.contents:
-            for child in soup_elem.contents:
-                child_buff, img_count = get_text(child, img_count)
-                buff += child_buff
-        elif soup_elem.name == "img":
+        if soup_elem.name == 'img':
             try:
-                img_url = soup_elem.attrs["src"]
-                if img_url[0] == "/":
-                    img_url = AKP_DOMAIN + img_url
-                img_count += 1
-                buff += "\n\n[image_%02d](%s)\n\n" % (img_count, img_url)
+                imgPath = soup_elem.attrs['src']
+                imgName = imgPath[imgPath.rfind('/')+1:imgPath.rfind('.')]
+
+                if imgPath[0] == '/':
+                    imgPath = AKP_DOMAIN + imgPath
+                buff += "{}[Image: {}]({}){}".format(WHITESPACE, imgName, imgPath, WHITESPACE)
             except KeyError:
                 pass
+        elif soup_elem.name == 'iframe' and 'src' in soup_elem.attrs:
+            title, link = get_youtube(soup_elem)
+            if title and link:
+                buff += "{}[{}]({}){}".format(WHITESPACE, title, link, WHITESPACE)
+        elif soup_elem.contents:
+            prefix = ""
+            suffix = ""
+            if soup_elem.name == 'a':
+                try:
+                    href = soup_elem.attrs['href']
+                    prefix = "["
+                    suffix = "]({})".format(href)
+                except KeyError:
+                    pass
+            elif soup_elem.name == 'em':
+                prefix = "*"
+                suffix = "*"
+            elif soup_elem.name == 'strong':
+                prefix = "**"
+                suffix = "**"
+            for child in soup_elem.contents:
+                child_buff = get_text(child)
+                if not child_buff.isspace():
+                    child_buff = prefix + child_buff + suffix
+                buff += child_buff
         elif soup_elem.name == "br":
-            buff += "\n\n"
-    return buff, img_count
+            buff += WHITESPACE
+    return buff
 
 
 def get_article(url):
